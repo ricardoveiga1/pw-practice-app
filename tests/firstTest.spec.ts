ac05e6bc98ac439dac1af2db266fa8b1da093fba
ricardoveiga1 <ricardoveiga.ti@gmail.com>
2025-02-11 22:25:25 -0300

    suite test

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,10 +1,34 @@
 import {test} from 'playwright/test'
 
-
-test('the firts test', async({page}) => {
+test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200/')
-    await page.getByText('Forms').click()
-    await page.getByText('Form Layouts').click()
-
-
 })
+
+test.describe('suite1', () => {
+    test.beforeEach(async({page}) => {
+        await page.getByText('Charts').click()
+    })
+    
+    test('the firts test1', async({page}) => {
+        await page.getByText('Form Layouts').click()
+    })
+    
+    test('navigate to datepicker page1', async({page}) => {  
+        await page.getByText('Datepicker').click()
+    })
+})
+
+test.describe('suite2', () => {
+    test.beforeEach(async({page}) => {
+        await page.getByText('Forms').click()
+    })
+    
+    test('the firts test2', async({page}) => {
+        await page.getByText('Form Layouts').click()
+    })
+    
+    test('navigate to datepicker page2', async({page}) => {  
+        await page.getByText('Datepicker').click()
+    })
+})
+
